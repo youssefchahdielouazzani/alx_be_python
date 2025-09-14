d80d1619afa5e4a8f7f41fb3cbeee517d8c671ee
unknown <youssefchahdielouazzani@gmail.com>
2025-09-14 17:25:44 +0100

    Ajout du script basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Définition des variables
+nombre1 = 10
+nombre2 = 5
+
+# Calculs
+somme = nombre1 + nombre2
+difference = nombre1 - nombre2
+produit = nombre1 * nombre2
+
+# Affichage des résultats
+print(f"L'addition de {nombre1} et {nombre2} donne {somme}")
+print(f"La soustraction de {nombre1} et {nombre2} donne {difference}")
+print(f"La multiplication de {nombre1} et {nombre2} donne {produit}")
