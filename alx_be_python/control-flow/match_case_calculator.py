504f43a9c71f5ac4106ecb998953276fdcbf3f53
youssef chahdi el ouazzani <youssefchahdielouazzani@gmail.com>
2025-09-17 03:29:33 -0700

    Ajout du fichier match_case_calculator.py

--- alx_be_python/control-flow/match_case_calculator.py
+++ alx_be_python/control-flow/match_case_calculator.py
@@ -1,28 +1,17 @@
-# match_case_calculator.py
-
-# Demander les deux nombres
 num1 = float(input("Saisir le premier nombre : "))
 num2 = float(input("Saisir le deuxième nombre : "))
-
-# Demander l'opération
 operation = input("Choisissez l'opération (+, -, *, /) : ")
 
-# Effectuer le calcul avec if / elif / else
 if operation == "+":
-    result = num1 + num2
-    print(f"Le résultat est {result}.")
+    print(f"Le résultat est {num1 + num2}.")
 elif operation == "-":
-    result = num1 - num2
-    print(f"Le résultat est {result}.")
+    print(f"Le résultat est {num1 - num2}.")
 elif operation == "*":
-    result = num1 * num2
-    print(f"Le résultat est {result}.")
+    print(f"Le résultat est {num1 * num2}.")
 elif operation == "/":
     if num2 == 0:
         print("Division par zéro impossible.")
     else:
-        result = num1 / num2
-        print(f"Le résultat est {result}.")
+        print(f"Le résultat est {num1 / num2}.")
 else:
     print("Opération invalide.")
-:wq
