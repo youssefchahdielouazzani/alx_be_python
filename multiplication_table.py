3c6767cff9cbc9b81f5c8b975b1baf7a02be4ba9
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-09-22 06:12:02 +0100

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,15 +1,2 @@
-# multiplication_table.py
 
-# Demander un nombre à l'utilisateur
-number = int(input("Enter a number to see its multiplication table: "))
-
-# Générer et afficher la table de multiplication
-# Demander à l'utilisateur de saisir un nombre
-number = int(input("Enter a number to see its multiplication table: "))
-
-# Générer et afficher la table de multiplication de 1 à 10
-
-for i in range(1, 11):
-    result = number * i
-    print(f"{number} * {i} = {result}")
 
