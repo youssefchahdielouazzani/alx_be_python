cabd0ad691bfa28ed49465d1fe394a4664d48a41
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-09-27 08:55:12 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("Shopping List Manager")
+    print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -7,23 +7,36 @@ def display_menu():
 
 def main():
     shopping_list = []
+
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
-        if choice == 1:
-            item = input("Enter item to add: ")
+        if choice == '1':
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
-        elif choice == 2:
-            item = input("Enter item to remove: ")
+            print(f"{item} has been added to the shopping list.")
+
+        elif choice == '2':
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
-        elif choice == 3:
-            for item in shopping_list:
-                print(item)
-        elif choice == 4:
+                print(f"{item} has been removed from the shopping list.")
+            else:
+                print(f"{item} is not in the shopping list.")
+
+        elif choice == '3':
+            if shopping_list:
+                print("\nShopping List:")
+                for i, item in enumerate(shopping_list, start=1):
+                    print(f"{i}. {item}")
+            else:
+                print("The shopping list is empty.")
+
+        elif choice == '4':
             print("Goodbye!")
             break
+
         else:
             print("Invalid choice. Please try again.")
 
