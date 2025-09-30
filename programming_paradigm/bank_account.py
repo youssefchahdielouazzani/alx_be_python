8ccca806284ea00a1873a2e64a620d0d47cdc4a1
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-09-30 14:05:38 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: {self.__account_balance}")
